50839091d7bee80087ddc4404272d4f07be1e568
fercho_03 <ingfercho03@gmail.com>
2025-05-25 23:56:52 -0500

    updates files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+requirements_file = 'requirements.txt'
+
+if os.path.isfile(requirements_file):
+    with open(requirements_file, 'r') as f:
+        required_dependencies = [line.strip() for line in f]
+else:
+    required_dependencies = []
+
+setup(
+    name="ETo_SM_Module",  
+    version="0.1.0",  
+    description="Module for Retrieving and Forecasting ETo and Soil Moisture Using Remote Sensing",  
+    author="Diego Rodriguez",  
+    author_email='diego.rodriguez-t@mail.escuelaing.edu.co',
+    url='https://github.com/ingfercho03/soil_ETo_forecast_module.git',
+    license="MIT",  
+    packages=['ETo_SM_Module'],
+    install_requires=required_dependencies,
+)
